c49a3eb053302c25e254b827ce67254a681cf1d2
Prafull044 <prafulldokrimare@gmail.com>
2026-06-24 18:57:06 +0530

    code
    java

---
+++ Backend/Operator1.java
@@ -0,0 +1,58 @@
+class Operator1 {
+    public static void main(String[] args) {
+
+        // byte s1 = 10;
+        // byte s2 = 50;
+        // int sub = s1 * s2;
+
+        // System.out.println(sub); // int is correct
+
+        // short s3 = 1330;
+        // short s4 = 5330;
+        // int sub2 = s3 * s4;
+
+        // System.out.println(sub2); // int is correct
+
+
+        // int s1 = 10;
+        // int s2 = 50;
+        // int sub = s1 * s2;
+
+        // System.out.println(sub);  // int is correct
+
+
+        
+        // long s1 = 104343;
+        // long s2 = 23420;
+        // long sub = s1 * s2;
+
+        // System.out.println(sub);  // long is correct
+
+
+        
+        // char s1 = 'a';
+        // char s2 = 'e';
+        // int sub = s1 * s2;
+
+        // System.out.println(sub);  // int is correct
+
+        
+        // float s1 = 10.0f;
+        // float s2 = 50.9f;
+        // float sub = s1 * s2;
+
+        // System.out.println(sub);  // float is correct
+
+
+        String s1 = "Prafull";
+        String s2 = "Kalash";
+        String sub = s1 - s2;
+
+        System.out.println(sub);  // float is correct
+
+
+
+        
+ 
+    }
+}
